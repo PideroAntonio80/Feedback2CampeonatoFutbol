4d1e39853e5d3f21697398bf06c95493c64af41d
Pedro Oros <shadycreekrd@gmail.com>
2020-08-08 18:03:47 +0200

    Incluido menu de opciones de comienzo de app

--- src/main/java/com/sanvalero/feedback2/Main.java
+++ src/main/java/com/sanvalero/feedback2/Main.java
@@ -1,9 +1,25 @@
 package com.sanvalero.feedback2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Campeonato de Fútbol");
+		System.out.println("Campeonato de Fútbol");
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("¿Desea comenzar? Responda Si o No");
+			String respuesta = sc.next();
+			
+			switch (respuesta.toUpperCase()) {
+				case "SI":
+					System.out.println("Introduzca nombre de usuario y contraseña, por favor");
+					break;
+				case "NO":
+					System.out.println("Hasta pronto");
+			}
+			
+		
 
 	}
 
